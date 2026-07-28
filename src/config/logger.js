3efe001379fcad33951bec576fa9772decc247eb
paarth293 <i.m.paarthgupta@gmail.com>
2026-07-28 11:31:22 +0530

    added the logging rule/logic: src/config/logger

---
+++ src/config/logger.js
@@ -0,0 +1,29 @@
+import winston from 'winston';
+import { env } from './env.js';
+
+const { combine, timestamp, printf, colorize, errors } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+export const logger = winston.createLogger({
+  level: env.isDev ? 'debug' : 'info',
+  format: combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }),
+    logFormat
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: combine(colorize(), logFormat),
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: 'logs/combined.log',
+    }),
+  ],
+});
